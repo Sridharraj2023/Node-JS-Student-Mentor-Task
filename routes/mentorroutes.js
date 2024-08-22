eb215650f5893396b16ea6fdeaf583819e4b75ee
Sridharraj2023 <sridharraj0110@gmail.com>
2024-08-22 09:28:33 +0530

    Student routes

--- routes/mentorroutes.js
+++ routes/mentorroutes.js
@@ -21,3 +21,34 @@ router.post('/', async (req, res) => {
 });
 
 //Route to assign students to mentor
+
+router.post('/:id/students', async(req, res)=> {
+    try {
+
+        const mentor = await assignStudentsToMentor(req.params.id, req.body.studentIds);
+        return successResponse(res, mentor, 'Students assigned to mentor successfully');
+        
+    } catch (error) {
+
+        return errorResponse(res, error);
+        
+    }
+});
+
+//Route to get all students assigned to a mentor
+
+router.get('/:id/students', async (req, res) => {
+
+    try {
+
+        const students = await getStudentsForMentor(req.params.id);
+        return successResponse(req, students, 'Students retrived successfully');
+        
+    } catch (error) {
+
+        return errorResponse(res, error);
+        
+    }
+});
+
+module.exports = router;
